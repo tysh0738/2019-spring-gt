a98119be75261e11f83694be197b423f606e5a4f
Sylvia Tseng <tysh0738@gmail.com>
2019-06-13 23:58:57 +0800

    find shortest path

--- main.cc
+++ main.cc
@@ -15,33 +15,35 @@ using namespace std;
 // create NetworkManager first
 NetworkManager *nm = new NetworkManager();
 map<string, vector<Edge *>> adv;
-vector<Edge *> bfs_paths;
-vector<Vertex *> existed_vertices;
+//vector<Edge *> bfs_paths;
+//vector<Vertex *> existed_vertices;
 vector<string> vertex_name;
 vector<string> odd_vertex;
 vector<int> degree;
-queue<Edge *> bfs_search;
+//queue<Edge *> bfs_search;
 
 void bfs(string src);
 
 int main(int argc, char** argv){
 
     // 1.create and print graph
-    nm->interpret("./topo.txt");
+    nm->interpret("./topo3.txt");
     nm->print_all_e();
     nm->print_all_v();
 
     // 2.odd or even degree
+    // record vetices' name
     Vertex *header;
     for(int i=0;i<nm->tablesize;i++){
         if(nm->vlist[i] != NULL) {
             header=nm->vlist[i];
             vertex_name.push_back(header->name);
-            //cout<<edge_name[i]<<endl;
+            //cout<<vertex_name[i]<<endl;
          }
          else break;
     }
 
+    // calculate degree
     cout<<"==========================================="<<endl<<"degree:"<<endl;
 
     int *degree;
@@ -68,11 +70,10 @@ int main(int argc, char** argv){
     for(int i=0;i<odd_vertex.size();i++)
         cout<<odd_vertex[i]<<" ";
 
-    //cout<<endl<<"index_odd:"<<index_odd;
     cout<<endl<<endl<<"degree finish"<<endl<<"============================================="<<endl;
 
-    // 3.find maximum matching of odd vertices and add edges
-    Edge *elist = nm->elist; 
+    // 3.find maximum matching of odd vertices and add edges (if there are odd degree's vertice)
+if(odd_vertex.size() != 0){
 
     Path *path;
     path = new Path();
@@ -81,7 +82,7 @@ int main(int argc, char** argv){
 
     vector<int> path_length[odd_vertex.size()-1];
     vector<string> path_vertex[odd_vertex.size()-1];
-   
+
     // find all matching, and save result in path_length and path_vertex
     for(int i=1;i<odd_vertex.size();i++){
         //matching: part1
@@ -100,41 +101,30 @@ int main(int argc, char** argv){
             path_vertex[i-1].push_back(avail_path[shorest_path][j]->head->name);
         path_vertex[i-1].push_back(odd_vertex[i]);
 
-        /*cout<<"path_vertex: ";
-        for(int j=0;j<=avail_path[shorest_path].size();j++)
-            cout<<path_vertex[i-1][j]<<" ";
-        cout<<endl;
-        */
-
         //matching: part2
-        int s=( (i+1) > (odd_vertex.size()-1) )?(i+2-odd_vertex.size()):(i+1);
-        int t=( (i+2) > (odd_vertex.size()-1) )?(i+3-odd_vertex.size()):(i+2);
-        avail_path = path->find_paths(std::string(odd_vertex[s]), std::string(odd_vertex[t]));
-        //path->debug();
+        if(odd_vertex.size()>2){
+            int s=( (i+1) > (odd_vertex.size()-1) )?(i+2-odd_vertex.size()):(i+1);
+            int t=( (i+2) > (odd_vertex.size()-1) )?(i+3-odd_vertex.size()):(i+2);
+            avail_path = path->find_paths(std::string(odd_vertex[s]), std::string(odd_vertex[t]));
+            //path->debug();
 
-        shorest_path = 0;
-        for(int j=0;j<avail_path.size();j++){ // choose shorest path
-           if(avail_path[j].size()<avail_path[0].size()) shorest_path = j;
-        }
+            shorest_path = 0;
+            for(int j=0;j<avail_path.size();j++){ // choose shorest path
+               if(avail_path[j].size()<avail_path[0].size()) shorest_path = j;
+            }
 
-        //cout<<"shorest path:" <<shorest_path<<endl;
+            //cout<<"shorest path:" <<shorest_path<<endl;
 
-        path_length[i-1].push_back(avail_path[shorest_path].size());
-        //cout<<"path_length: "<<path_length[i-1][1]<<endl;
+            path_length[i-1].push_back(avail_path[shorest_path].size());
+            //cout<<"path_length: "<<path_length[i-1][1]<<endl;
 
-        for(int j=0; j<avail_path[shorest_path].size();j++)
-            path_vertex[i-1].push_back(avail_path[shorest_path][j]->head->name);
-        path_vertex[i-1].push_back(odd_vertex[t]);
-
-        /*cout<<"path_vertex: ";
-        for(int j=0;j<=path_vertex[i-1].size();j++)
-            cout<<path_vertex[i-1][j]<<" ";
-        cout<<endl;
-        */
-        
+            for(int j=0; j<avail_path[shorest_path].size();j++)
+                path_vertex[i-1].push_back(avail_path[shorest_path][j]->head->name);
+            path_vertex[i-1].push_back(odd_vertex[t]);
+      
         // calculate total path length
         path_length[i-1].push_back(path_length[i-1][0]+path_length[i-1][1]);
-
+        }
     }
 
     cout<<"path_length:"<<endl;
@@ -153,79 +143,86 @@ int main(int argc, char** argv){
 
     //maximum matching
     int max_matching=0;
-    for(int i=1;i<odd_vertex.size()-1;i++){
-       if(path_length[i][2]<path_length[max_matching][2])
-           max_matching = i;
+    if(odd_vertex.size()>2){
+        for(int i=1;i<odd_vertex.size()-1;i++){
+           if(path_length[i][2]<path_length[max_matching][2])
+               max_matching = i;
+        }
     }
     //cout<<"max_matching:" <<max_matching<<endl;
 
-    // add edge for reapted path
+    // add edge for reapted path and set capacity to 3
     for(int i=0;i<path_length[max_matching][0];i++){
        nm->connect_r(std::string(path_vertex[max_matching][i]),std::string(path_vertex[max_matching][i+1]));
+       nm->get_edge(std::string(path_vertex[max_matching][i]),std::string(path_vertex[max_matching][i+1]))->set_cap(3);
+       nm->get_edge(std::string(path_vertex[max_matching][i+1]),std::string(path_vertex[max_matching][i]))->set_cap(3);
     }
 
-    for(int i=0;i<path_length[max_matching][1];i++){
-       nm->connect_r(std::string(path_vertex[max_matching][i+path_length[max_matching][0]+1]),std::string(path_vertex[max_matching][i+path_length[max_matching][0]+2]));
+    if(odd_vertex.size()>2){
+        for(int i=0;i<path_length[max_matching][1];i++){
+           nm->connect_r(std::string(path_vertex[max_matching][i+path_length[max_matching][0]+1]),std::string(path_vertex[max_matching][i+path_length[max_matching][0]+2]));
+           nm->get_edge(std::string(path_vertex[max_matching][i+path_length[max_matching][0]+1]),std::string(path_vertex[max_matching][i+path_length[max_matching][0]+2]))->set_cap(3);
+           nm->get_edge(std::string(path_vertex[max_matching][i+path_length[max_matching][0]+2]),std::string(path_vertex[max_matching][i+path_length[max_matching][0]+1]))->set_cap(3);
     }
-
-    nm->print_all_e();
-    nm->print_all_v();
-
-    // 4.find postman shortest cycle
- 
-   // build the edges belongs to vertex
-   /* while(elist!=NULL){
-        adv[elist->head->name].push_back(elist);
-        elist=elist->next;
     }
-
-    //BFS
-    existed_vertices.push_back(nm->get_node("e"));
-    bfs("e");
-    while(!bfs_search.empty()){
-        bfs(bfs_search.front()->tail->name);
-        bfs_search.pop();
+    //nm->print_all_e();
+    //nm->print_all_v();
+}
+    // 4.find chinese postman shortest circuit
+    // calcuate the number of edges in new graph(that is minmum path length*2)     
+    Edge *traversal = nm->elist;
+    int min_length = 0;
+    while(traversal!=NULL) {
+        min_length++;
+        traversal=traversal->next;
     }
+    min_length=min_length/2; // because use two di-edge to implement non-direction edge
+    //cout<<"minmum path length: "<<min_length<<endl;
 
-    Edge *bfs_elist=NULL, *tmp=bfs_elist;
+    // find shorest circuit
+    vector<string> circuit; // record vertices on the shorest circuit
+    int current = 0; // circuit begin at vertex_name[0]
+    int next_temp = 0; // be next vertex if no connected edge's capacity=3
+    int check = 0;
+    circuit.push_back(vertex_name[current]);
 
-    cout << "BFS: " << endl;
-    for(int i=0;i<bfs_paths.size();i++){
-        cout << bfs_paths.at(i)->head->name << "->" << bfs_paths.at(i)->tail->name << endl;
-        if(tmp==NULL){
-            bfs_elist = new Edge(bfs_paths.at(i));
-            tmp = bfs_elist;
-        } else {
-            tmp->next = new Edge(bfs_paths.at(i));
-            tmp = tmp->next;
+    for(int i=0;i<min_length;i++){
+        check=0;
+        for(int j=0;j<vertex_name.size();j++){
+            if( !nm->connected(std::string(vertex_name[current]),std::string(vertex_name[j])) ){
+                if(nm->get_edge(std::string(vertex_name[current]),std::string(vertex_name[j]))->cap == 3){
+                    circuit.push_back(vertex_name[j]);
+                    nm->disconnect(std::string(vertex_name[current]),std::string(vertex_name[j]));
+                    nm->disconnect(std::string(vertex_name[j]),std::string(vertex_name[current]));
+                    current=j;
+                    check=1;
+                    break;
+                }
+                else 
+                    next_temp = j;
+            }
         }
-    }*/
+        if(check==0){
+            circuit.push_back(vertex_name[next_temp]);
+            nm->disconnect(std::string(vertex_name[current]),std::string(vertex_name[next_temp]));
+            nm->disconnect(std::string(vertex_name[next_temp]),std::string(vertex_name[current]));
+            current = next_temp;  
+        }
+        //nm->print_all_e();        
+    }
 
+    // print result
+    cout<<"========================================="<<endl;
+    cout<<"        POSTMAN SHORSTEST CIRCUIT        "<<endl;
+    cout<<"========================================="<<endl;
+    cout<<"minmum path length: "<<min_length<<endl;
 
-   /* Gplot *gp = new Gplot();
-    gp->gp_add(nm->elist);
-    gp->gp_dump(true);
-    gp->gp_export("bfs");
-   */
+    cout<<"path:"<<endl<<circuit[0];
+    for(int i=1;i<circuit.size();i++){
+       cout<<" -> "<<circuit[i];
+    }cout<<endl;
+
     nm->clear();
 
     return 0;
-}
-
-void bfs(string src){
-    for(int i=0;i<adv[src].size();i++){
-        // cout << adv[src].at(i)->head->name << "=>" << adv[src].at(i)->tail->name << endl;
-        // check the tail is in the existed_vertices or not
-        if(find(bfs_paths.begin(), bfs_paths.end(), adv[src].at(i))==bfs_paths.end()){
-            // if tail is not existed, then push into existed vertices
-            if(find(existed_vertices.begin(), existed_vertices.end(), adv[src].at(i)->tail)==existed_vertices.end()){
-                // and push this edge into bfs result
-                bfs_paths.push_back(adv[src].at(i));
-                // not existed, then push into existed
-                existed_vertices.push_back(adv[src].at(i)->tail);
-                // push
-                bfs_search.push(adv[src].at(i));
-            }
-        }
-    }
 }
